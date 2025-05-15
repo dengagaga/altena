5930f3697fcdac67d7e225850a219ee3259843bf
hexplode13 <hexplode13@gmail.com>
2025-05-15 12:23:12 +0300

    Формы

--- README.md
+++
@@ -1,1 +0,0 @@
-# VektorStal.github.io
--- ad2.js
+++ ad2.js
@@ -1,14 +1,14 @@
-const header = document.querySelector('.header')
-const dropMobil = document.querySelector('.drop-mobil')
-const headerBurgerBtn = document.querySelector('.header_burger-btn')
-const navLinkDropMobil = document.querySelector('.nav_link--drop-mobil')
-headerBurgerBtn.onclick = () => {
-  header.classList.toggle('open')
-}
-navLinkDropMobil.onclick = () => {
-  dropMobil.classList.toggle('drop-mobil--active')  
-  navLinkDropMobil.classList.toggle('navLinkDropMobil--active')  
-}
+// const header = document.querySelector('.header')
+// const dropMobil = document.querySelector('.drop-mobil')
+// const headerBurgerBtn = document.querySelector('.header_burger-btn')
+// const navLinkDropMobil = document.querySelector('.nav_link--drop-mobil')
+// headerBurgerBtn.onclick = () => {
+//   header.classList.toggle('open')
+// }
+// navLinkDropMobil.onclick = () => {
+//   dropMobil.classList.toggle('drop-mobil--active')  
+//   navLinkDropMobil.classList.toggle('navLinkDropMobil--active')  
+// }
 
 
 
@@ -21,4 +21,12 @@ navLinkDropMobil.onclick = () => {
 //   }
 // })
 
-
+const modalClose = document.querySelector('.modal__close')
+const modalBtn = document.querySelector('.header_left-btn-2')
+const modal = document.querySelector('.modal')
+modalClose.onclick = () => {
+  modal.classList.remove('modal--active')
+}
+modalBtn.onclick = () => {
+  modal.classList.add('modal--active')
+}
